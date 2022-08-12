1bf5675bd3e7b8a25609f912a57b9e700061506b
akankshagoel28 <akanksha.goel2020@vitstudent.ac.in>
2022-08-12 15:23:13 +0530

    service screen coded

---
+++ src/Dialog.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import ServiceDetails from './serviceDetails';
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
+const BootstrapDialogTitle = (props) => {
+  const { children, onClose, ...other } = props;
+
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+};
+
+BootstrapDialogTitle.propTypes = {
+  children: PropTypes.node,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default function CustomizedDialogs() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div style={{borderRadius:"15px"}}>
+      <ServiceDetails handleClick={handleClickOpen} />
+      {/* <Button variant="outlined" className="Request" onClick={handleClickOpen}>
+       Request
+      </Button> */}
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <DialogContent dividers
+        >
+          <div>
+            <form>
+              <div className="leftAlign"><h1>GET A QUOTATION</h1></div>
+              <input type="text" id="name" name="name" placeholder="Full Name"></input>
+              <input type="email" id="email" name-="email" placeholder="Email"></input>
+              <input type="tel" id="phone1" name="phoneno1" placeholder="Phone No."></input>
+              <input type="tel" id="phone2" className="small" name="phoneno2" placeholder="Phone No."></input>
+              <input type="tel" id="phone3" name="phoneno3" className="small" placeholder="Phone No."></input>
+              <p>Project Description</p>
+              <textarea rows="6" column="50" placeholder="Type here..."></textarea>
+              <div className="buttons"><button style={{ background: "#F2F3FA", color: "#3F4AAF" }}>Cancel</button>
+                <button style={{ background: "#3F4AAF", color: "#FFFFFF" }}>Request</button>
+              </div>
+            </form>
+          </div>
+        </DialogContent>
+      </BootstrapDialog>
+    </div>
+  );
+}
---
+++ src/serviceDetails.js
@@ -0,0 +1,46 @@
+import React from "react";
+import laptop from './laptop.png'
+// import polygon1 from './Polygon1.svg'
+// import polygon2 from './Polygon2.svg'
+import statsLine from './line-stats.svg'
+import statsSolid from './solid-stats.svg'
+import homeSolid from './filled-home.svg'
+import homeLine from './line-home.svg'
+import claritySolid from './clarity_help-solid.svg'
+import clarityLine from './clarity_help-line.svg'
+import logOut from './log-out.svg'
+import arrow from './bi_arrow-down.svg'
+export default function ServiceDetails(props) {
+    return (<div className="main">
+        {/* <img src={polygon1} alt="" className="polygon1"></img> */}
+        {/* <img src={polygon2} alt="" className="polygon2"></img> */}
+        <div className="leftBar"></div>
+        <div className="sideBar">
+        <img src={homeLine} className="home" alt="" onMouseOver={e => (e.currentTarget.src = {homeSolid})}></img>
+        <img src={statsLine} alt="" className="stats" onMouseOver={e => (e.currentTarget.src = {statsSolid})}></img>
+        <img src={clarityLine} alt="" className="clarity" onMouseOver={e => (e.currentTarget.src = {claritySolid})}></img>
+        <img src={logOut} className="logOut" alt=""></img>
+        </div>
+        <div className="centerBox"> 
+            <div class="info">
+                <img  className="arrow" src={arrow} alt=""></img>
+                <h1>SECURITY AUDITING</h1>
+                <button className="price">$12,000</button>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit. Etiam eu turpis molestie, dictum est a,
+                    mattis tellus. Sed dignissim, metus nec fringilla accumsan,
+                    risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas
+                    eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu
+                    ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent
+                    auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse
+                    ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet
+                    lacinia. Aliquam in elementum tellus.
+                </p>
+                <button className="Request" variant="outlined" onClick={props.handleClick}>Request</button>
+            </div>
+                <img src={laptop} alt="" className="laptop"></img>
+        </div>
+        <div className="rightBar"></div>
+    </div>
+    )
+}
